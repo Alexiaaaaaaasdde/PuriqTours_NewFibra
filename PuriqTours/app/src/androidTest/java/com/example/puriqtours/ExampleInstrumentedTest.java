8e962eb4468a84c5cd7d75069b0fbfb68c66f23b
markito <a20193265@pucp.edu.pe>
2025-09-17 13:37:33 -0500

    jdshhh

--- PuriqTours/app/src/androidTest/java/com/example/puriqtours/ExampleInstrumentedTest.java
+++ PuriqTours/app/src/androidTest/java/com/example/puriqtours/ExampleInstrumentedTest.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
 import static org.junit.Assert.*;
 
 /**
- * Instrumented test,jjjj whichhhh will execute on an Android device.
+ * hhhhhhhhInstrumented test,jjjj whichhhh will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
