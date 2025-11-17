02f6ee54779b849c2f067d47348f1c612ebd4d34
markito <a20193265@pucp.edu.pe>
2025-11-17 01:08:19 -0500

    g

--- PuriqTours/app/src/main/java/com/example/puriqtours/superadmin/LogItem.java
+++ PuriqTours/app/src/main/java/com/example/puriqtours/superadmin/LogItem.java
@@ -4,10 +4,12 @@ public class LogItem {
     public String tipo;
     public String fecha;
     public String descripcion;
+    public long timestamp; // En milisegundos para ordenar
 
     public LogItem(String tipo, String fecha, String descripcion) {
         this.tipo = tipo;
         this.fecha = fecha;
         this.descripcion = descripcion;
+        this.timestamp = 0;
     }
 }
